2198b6bee5f4e9d5dbf6ff61bc1c9d9aaa3b54f6
smarin <sara.marin.pino@accenture.com>
2020-10-05 18:13:46 +0200

    pass data via props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Main from './Main';
 import Sidebar from './Sidebar'
+import store from './store'
+import  _ from 'lodash';
 
 const App = () => {
+  const { contacts } = store.getState();
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={_.values(contacts)} />
       <Main />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
+import App from "./App";
+// import registerServiceWorker from "./serviceWorker";
+import store from "./store";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const render = () => {
+  fancyLog();
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+render();
+store.subscribe(render);
+// registerServiceWorker();
+
+function fancyLog() {
+  console.log("%c Rendered with ? ??", "background: purple; color: #fff");
+  console.log(store.getState());
+}
