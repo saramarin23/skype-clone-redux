15648c933356a7782fd4856f63510a4d1bec4826
smarin <sara.marin.pino@accenture.com>
2020-10-05 22:25:38 +0200

    refactoring to set initial state from the reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,6 @@ import { createStore } from "redux";
 import reducer from "../reducers";
 import { contacts } from "../static-data";
 
-const store = createStore(reducer, {contacts})
+const store = createStore(reducer)
 
 export default store
