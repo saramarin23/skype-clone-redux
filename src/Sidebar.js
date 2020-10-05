2f3540813ed6534ff602e87842b64f7f1707e18d
smarin <sara.marin.pino@accenture.com>
2020-10-05 22:09:50 +0200

    sidebar layout

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import User from "./User"; 
+import User from "./User"; 
 import "./Sidebar.css";
 
 const Sidebar = ({ contacts }) => {
   return (
     <aside className="Sidebar">
-      {/* {contacts.map(contact => <User user={contact} key={contact.user_id} />)} */}
+      {contacts.map(contact => <User user={contact} key={contact.user_id} />)}
     </aside>
   );
 };
